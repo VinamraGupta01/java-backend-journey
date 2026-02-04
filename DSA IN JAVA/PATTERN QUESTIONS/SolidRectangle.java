ac3a4aa904fea2e9f42303788921d93db43986c2
Vinamra Gupta <vinamra.gupta.dev@gmail.com>
2026-02-04 13:47:51 +0530

    IMPLEMENTED SOLID RECTANGLE PROGRAM

---
+++ DSA IN JAVA/PATTERN QUESTIONS/SolidRectangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SolidRectangle {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+
+        int rows=sc.nextInt();
+        int cols=sc.nextInt();
+        
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
