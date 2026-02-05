adf8e8608423ce69ccf8d9aec7192dc4fdb716e4
Vinamra Gupta <vinamra.gupta.dev@gmail.com>
2026-02-05 13:38:30 +0530

    IMPLEMENTED PASCAL'S TRIANGLE PROBLEM

---
+++ DSA IN JAVA/PATTERN QUESTIONS/PascalTriangle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            //SPACES FOR ALIGNMENT
+            for (int s = 0; s < n - i - 1; s++) {
+                System.out.print(" ");
+            }
+
+            int value = 1;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(value + " ");
+                value = value * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
